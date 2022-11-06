78b99f91a7c11f9537d9cf2b76c70b03368b5384
atorres-io <atorresalcala.me@gmail.com>
2022-11-06 19:41:52 +0100

    build(version): add build version

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,10 @@
 import Signature from '@/components/signature'
 import Action from '@/components/action'
-import Breadcrumbs from '@/components/breadcrumbs'
 import './styles.css'
 
 const Home = () => {
   return (
     <div className="home">
-      <header>
-        <Breadcrumbs />
-      </header>
       <main>
         <Action />
       </main>
